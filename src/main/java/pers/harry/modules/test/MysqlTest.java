b74e80d5402cf392f03fa72f3bdcb8c9d288c8b6
andychow01 <andychow01@163.com>
2019-04-23 00:28:18 +0800

    delete

--- src/main/java/pers/harry/modules/test/MysqlTest.java
+++ src/main/java/pers/harry/modules/test/MysqlTest.java
@@ -7,53 +7,30 @@ public class MysqlTest {
         try{
             //调用Class.forName()方法加载驱动程序
             Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("成功加载MySQL驱动！");
+            System.out.println("load MySQL driver success");
 
-            String url="jdbc:mysql://localhost:3306/aniu";    //JDBC的URL
+            String url="jdbc:mysql://192.168.152.128:3306/employees";    //JDBC的URL
             Connection conn;
 
-            conn = DriverManager.getConnection(url,    "root","");
+            conn = DriverManager.getConnection(url, "zhou", "zhou");
             Statement stmt = conn.createStatement(); //创建Statement对象
-            System.out.println("成功连接到数据库！");
+            System.out.println("access database employees success");
 
             //查询数据的代码
-            String sql = "select * from stu";    //要执行的SQL
+            String sql = "select emp_no, first_name, last_name from employees limit 5;";    //要执行的SQL
             ResultSet rs = stmt.executeQuery(sql);//创建数据对象
-            System.out.println("编号"+"\t"+"姓名"+"\t"+"年龄");
+            System.out.println("emp_no"+"\t"+"first_name"+"\t"+"last_name");
             while (rs.next()){
                 System.out.print(rs.getInt(1) + "\t");
                 System.out.print(rs.getString(2) + "\t");
-                System.out.print(rs.getInt(3) + "\t");
-                System.out.println();
-            }
-
-            //修改数据的代码
-            String sql2 = "update stu set name=? where number=?";
-            PreparedStatement pst = conn.prepareStatement(sql2);
-            pst.setString(1,"8888");
-            pst.setInt(2,198);
-            pst.executeUpdate();
-
-            //删除数据的代码
-            String sql3 = "delete from stu where number=?";
-            pst = conn.prepareStatement(sql3);
-            pst.setInt(1,701);
-            pst.executeUpdate();
-
-            ResultSet rs2 = stmt.executeQuery(sql);//创建数据对象
-            System.out.println("编号"+"\t"+"姓名"+"\t"+"年龄");
-            while (rs.next()){
-                System.out.print(rs2.getInt(1) + "\t");
-                System.out.print(rs2.getString(2) + "\t");
-                System.out.print(rs2.getInt(3) + "\t");
+                System.out.print(rs.getString(3) + "\t");
                 System.out.println();
             }
 
             rs.close();
             stmt.close();
             conn.close();
-        }catch(Exception e)
-        {
+        } catch(Exception e) {
             e.printStackTrace();
         }
     }
